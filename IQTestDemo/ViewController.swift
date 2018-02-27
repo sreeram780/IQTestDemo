fe417f5acdbd210853b9a769f57fb258b0614547
SreenivasulaReddy <sreenivasulareddy@vlsys61-130.local>
2018-02-27 12:52:03 +0530

    Initial Commit

---
+++ IQTestDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IQTestDemo
+//
+//  Created by SreenivasulaReddy on 27/02/18.
+//  Copyright © 2018 Sreenivasulareddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
